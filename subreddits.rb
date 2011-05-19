e3b5753a29f084b7ce4c9c66eb9a851ea5144e26
OneWhoFrogs <onewhofrogs@gmail.com>
2011-05-18 22:37:37 -0500

    Hopefully the last of the escaping

--- subreddits.rb
+++ subreddits.rb
@@ -61,7 +61,8 @@ class Motorcycles < Subreddit
   
   def build_css(rows)
     css = rows.inject("") do |memo, row|
-      memo += ".id-t2_#{row["id"]}:after {color: gray; font-size: 0.75em; content: ' #{row['state']}' !important}\n"
+      state = row['state'].gsub(/'/, "\\\\'")
+      memo += ".id-t2_#{row["id"]}:after {color: gray; font-size: 0.75em; content: ' #{state}' !important}\n"
     end
   end
 end
@@ -77,7 +78,7 @@ class Autos < Subreddit
   def build_css(rows)
     css = rows.inject("") do |memo, row|
       state = row['state'].gsub(/'/, "\\\\'")
-      memo += ".id-t2_#{row["id"]}:after {color: gray; font-size: 0.75em; content: ' [#{row['state']}]' !important}\n"
+      memo += ".id-t2_#{row["id"]}:after {color: gray; font-size: 0.75em; content: ' [#{state}]' !important}\n"
     end
   end
 end
